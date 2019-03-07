b984d28a4a7e2baed7d750a92a4b66fcb584825c
ledda <robertledang@gmail.com>
2019-03-07 10:18:47 +0100

    #1 Changes to MVC arcitecture. Added model

---
+++ core/src/com/rngg/models/GameModel.java
@@ -0,0 +1,13 @@
+package com.rngg.models;
+
+public class GameModel {
+    private int playerScore = 0;
+
+    public int getPlayerScore() {
+        return playerScore;
+    }
+
+    public void setPlayerScore(int score) {
+        playerScore = score;
+    }
+}
